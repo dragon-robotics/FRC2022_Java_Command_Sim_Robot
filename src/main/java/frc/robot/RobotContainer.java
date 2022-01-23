87c5a898eacd18fa425c6d2a898435fdda098385
Doug Liu <dougdliu@gmail.com>
2022-01-23 00:13:12 -0700

    1. Removed example commands and subsystems 2. Template for both drivetrain and alt drivetrain implementation

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -5,8 +5,11 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.GenericHID;
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController;
+import frc.robot.commands.ArcadeDriveCommand;
 import frc.robot.commands.ExampleCommand;
+import frc.robot.subsystems.DrivetrainSubsystem;
 import frc.robot.subsystems.ExampleSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
 
@@ -18,12 +21,18 @@ import edu.wpi.first.wpilibj2.command.Command;
  */
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
-  private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
+  private final DrivetrainSubsystem m_drivetrainSubsystem = new DrivetrainSubsystem();
 
-  private final ExampleCommand m_autoCommand = new ExampleCommand(m_exampleSubsystem);
+  // Joysticks
+  private final Joystick m_driverController = new Joystick(Constants.DRIVER);
+  private final Joystick m_operatorController = new Joystick(Constants.OPERATOR);
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
+    
+    // Set default command to arcade drive when in teleop
+    m_drivetrainSubsystem.setDefaultCommand(getArcadeDriveCommand());
+
     // Configure the button bindings
     configureButtonBindings();
   }
@@ -43,6 +52,14 @@ public class RobotContainer {
    */
   public Command getAutonomousCommand() {
     // An ExampleCommand will run in autonomous
-    return m_autoCommand;
+    return null;
+  }
+
+  public Command getArcadeDriveCommand() {
+    // Commands //
+    return new ArcadeDriveCommand(
+        m_drivetrainSubsystem,
+        () -> -m_driverController.getRawAxis(Constants.STICK_LEFT_Y),
+        () -> m_driverController.getRawAxis(Constants.STICK_RIGHT_X));
   }
 }
--- src/main/java/frc/robot/commands/ArcadeDriveCommand.java
+++ src/main/java/frc/robot/commands/ArcadeDriveCommand.java
@@ -4,25 +4,45 @@
 
 package frc.robot.commands;
 
+import java.util.function.Supplier;
+
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.DrivetrainSubsystem;
 
 public class ArcadeDriveCommand extends CommandBase {
-  /** Creates a new ArcadeDriveCommand. */
-  public ArcadeDriveCommand() {
+
+  private final DrivetrainSubsystem m_drivetrain;
+  private final Supplier<Double> m_speed;
+  private final Supplier<Double> m_rotation;
+
+  /** Creates a new ArcadeDrive. */
+  public ArcadeDriveCommand(
+      DrivetrainSubsystem drivetrain,
+      Supplier<Double> speed,
+      Supplier<Double> rotation) {
+    m_drivetrain = drivetrain;
+    m_speed = speed;
+    m_rotation = rotation;
+
     // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(drivetrain);
   }
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
+  public void initialize() {
+  }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {}
+  public void execute() {
+    m_drivetrain.arcadeDrive(m_speed.get(), m_rotation.get());
+  }
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+  }
 
   // Returns true when the command should end.
   @Override
